2fa9bf2b35a761cd22a48229ffcae31f207c9dc4
Konrad Maciejczyk <conradmac97@gmail.com>
2020-10-31 17:43:42 +0100

    Added matrix determinant method

--- src/Macierz_testy.java
+++ src/Macierz_testy.java
@@ -4,13 +4,19 @@ public class Macierz_testy {
         // piotrek
 
         Macierz m = new Macierz();
-        System.out.println(m);
+       // System.out.println(m);
 
         // tomek
 
 
 
         // konrad
+
+        double d[][] = {{1, 3, 4}, {2,4,3}, {3, 4, 5}}; 
+        Macierz matrix = new Macierz(d);
+
+        System.out.println(matrix); //matrix.transpose();
+        System.out.println(matrix.determinant());
 
 
 
