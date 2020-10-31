a5f048a1a153ee46a35b2576854058fad1c99c8d
connetions <connetions22@gmail.com>
2020-10-31 18:28:36 +0100

    testy

--- src/Macierz_testy.java
+++ src/Macierz_testy.java
@@ -1,28 +1,52 @@
 //Piotr Morel, Konrad Maciejczyk, Tomasz Tomżyński, Java, gr 2, 2020
+
+import javax.crypto.Mac;
+
+
 public class Macierz_testy {
 
     public static void main(String []args){
         // piotrek
 
+        System.out.println("Tworzenie macierzy wypełnionej zerami");
         Macierz m = new Macierz();
-       // System.out.println(m);
+        System.out.println(m);
 
+        System.out.println("Iloczyn skalarny macierzy A i B");
+        System.out.println("Macierz A:");
         Macierz dot_product_A = new Macierz(
                 1,2,3,
                 4,5,6,
                 0,0,0
         );
+        System.out.println(dot_product_A);
 
+        System.out.println("Macierz B:");
         Macierz dot_product_B = new Macierz(
                 7, 8, 0,
                 9, 10,0,
                 11,12,0
         );
+        System.out.println(dot_product_B);
 
+        System.out.println("Wynik iloczynu skalarnego");
         Macierz result = dot_product_A.matrix_multiply(dot_product_B);
-        System.out.println(result);  // check https://www.mathsisfun.com/algebra/images/matrix-multiply-c.svg
+        System.out.println(result);
 
         // tomek
+
+        System.out.println("Test kopiowania macierzy");
+        Macierz copy_test_base = new Macierz(1);
+        System.out.println("Macierz bazowa");
+        System.out.println(copy_test_base);
+        Macierz copy_test_copied = copy_test_base.copy();
+        System.out.println("Kopia macierzy:");
+        System.out.println(copy_test_copied);
+        copy_test_base.scale(-1);
+        System.out.println("Zmiana w macierzy bazowej:");
+        System.out.println(copy_test_base);
+        System.out.println("Kopia macierzy bazowej");
+        System.out.println(copy_test_copied);
 
         System.out.println("Dodawanie macierzy m1 oraz m2:");
         Macierz m1, m2;
