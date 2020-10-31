293fb309292a05d33668bb3dedf0769f36013ef2
Konrad Maciejczyk <conradmac97@gmail.com>
2020-10-31 16:53:17 +0100

    Added String toString method

--- src/Macierz_testy.java
+++ src/Macierz_testy.java
@@ -1,6 +1,11 @@
 public class Macierz_testy {
 
     public static void main(String []args){
+        double macierz[][] = {{1, 3, 4}, {2, 4, 3}, {3, 4, 5}};
+
+        Macierz matrix = new Macierz(macierz);
+
+        
 
         
     }
