b3104ff9b0179a5a2f33f98f130f8cb5b8cd63d9
Piotrenewicz <piotrekm9@onet.eu>
2020-12-07 20:24:34 +0100

    5.5copied

---
+++ zad5/5.5/Odbior.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class Odbior extends Thread
+{
+    Socket sock;
+    BufferedReader sockReader;
+    Boolean alive;
+
+    public Odbior(Socket sock) throws IOException
+    {
+        this.sock=sock;
+        this.sockReader=new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        this.alive = true;
+    }
+
+    public void run()
+    {
+        String str;
+        while (this.alive){
+            try {
+                str = sockReader.readLine();
+                System.out.println("<Nadeszlo:> " + str);
+                if(str.equals("end")){
+                    this.alive=false;
+                }
+            } catch (IOException e) {
+                System.out.println("Połączenie zakończone.");
+            }
+        }
+    }
+
+    public void end(){
+        this.alive = false;
+        this.interrupt();
+    }
+}
